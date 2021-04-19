b2eea3c3f2aba613428f991145f2091445d98af3
achugun <aleksander.chugunoff@yandex.ru>
2021-04-19 12:21:18 +0300

    6

--- src/main/java/com/home/contacts/controllers/MainController.java
+++ src/main/java/com/home/contacts/controllers/MainController.java
@@ -1,18 +1,9 @@
 package com.home.contacts.controllers;
 
-import com.home.contacts.dto.AuthenticationRequestDto;
-import com.home.contacts.entity.UserEntity;
-import com.home.contacts.entity.enums.Status;
 import com.home.contacts.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import javax.validation.Valid;
 
 @Controller
 public class MainController {
@@ -31,25 +22,8 @@ public class MainController {
     }
 
     @GetMapping(value = "/login")
-    public String login(Model model) {
-        model.addAttribute("user", new AuthenticationRequestDto());
+    public String showLoginPage() {
         return "login";
-    }
-
-    @PostMapping(value = "/login")
-    public String authenticationUser(@RequestBody @Valid AuthenticationRequestDto requestDto,
-                                     BindingResult result) {
-        UserEntity user = userService.findByUsername(requestDto.getUsername());
-
-        if (!user.getStatus().equals(Status.ACTIVE)) {
-            result.rejectValue("username", null, "Your status is not 'Active'!");
-        }
-
-        if (result.hasErrors()) {
-            return "login";
-        }
-
-        return "index";
     }
 
     @GetMapping(value = "/403")
@@ -57,9 +31,9 @@ public class MainController {
         return "error/403";
     }
 
-    @GetMapping(value = "/banned")
+    /*@GetMapping(value = "/banned")
     public String banned() {
         return "error/banned";
-    }
+    }*/
 
 }
